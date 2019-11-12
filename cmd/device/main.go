3a663f14d002653d16f8289b112b25d863ab4b67
Joshua Adams <n7down@gmail.com>
2019-11-11 18:15:23 -0700

    udp

--- cmd/device/main.go
+++ cmd/device/main.go
@@ -1,1 +1,11 @@
 package main
+
+import (
+	"net"
+)
+
+func main() {
+	Conn, _ := net.DialUDP("udp", nil, &net.UDPAddr{IP: []byte{127, 0, 0, 1}, Port: 10001, Zone: ""})
+	defer Conn.Close()
+	Conn.Write([]byte("hello"))
+}
